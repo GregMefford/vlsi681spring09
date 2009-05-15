76a4c2f3626674d6aa0fc23c2aade5293c39f611
Megan Schneider <megan.m.schneider@gmail.com>
2009-05-15 15:23:13 -0400

    ALU changes, tested w/ DO file

---
+++ projects/ALU/work/@a@l@u/_primary.vhd
@@ -0,0 +1,12 @@
+library verilog;
+use verilog.vl_types.all;
+entity ALU is
+    port(
+        A               : in     vl_logic_vector(15 downto 0);
+        B               : in     vl_logic_vector(15 downto 0);
+        CONTROL         : in     vl_logic_vector(3 downto 0);
+        CC              : out    vl_logic_vector(2 downto 0);
+        Z               : out    vl_logic_vector(15 downto 0);
+        \OF\            : out    vl_logic_vector(15 downto 0)
+    );
+end ALU;
