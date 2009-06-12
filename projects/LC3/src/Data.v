c36504b85bd24b3c24b4f9a7bb9748dc3208d129
Greg Mefford <gregory.mefford@gmail.com>
2009-06-12 11:27:21 -0400

    Added some support for the LCD interface.

--- projects/LC3/src/Data.v
+++ projects/LC3/src/Data.v
@@ -19,14 +19,31 @@ output	[15:0] DATA;
 
 output	[15:0] MAR;
 
+wire LCD_ADDR;
+wire DRAM_WE;
+wire [15:0] DRAM_Data;
+reg  [15:0] LCD_Data;
+reg  [15:0] LCD_Cycles;
+reg  [15:0] CLK_Counter;
+wire [15:0] LCD_Counter;
+
+assign LCD_ADDR = (16'h1000 == MAR);
+assign DRAM_WE = (~LCD_ADDR && MEM_WE);
 assign MAR = (MAR_CONTROL) ? DATA : Y;
+assign DATA = (LCD_ADDR) ? DRAM_Data : LCD_Counter;
+
+always @(posedge CLK) begin
+	CLK_Counter = CLK_Counter + 1;
+	if(DRAM_WE) LCD_Cycles = CLK_Counter;
+	if(LCD_ADDR && MEM_WE) LCD_Data = RD_DATA;
+end
 
 DRAM	DRAM_inst (
 	.address ( MAR[11:0] ),
 	.clock ( CLK ),
 	.data ( RD_DATA ),
 	.wren ( WE ),
-	.q ( DATA )
+	.q ( DRAM_Data )
 );
 
 endmodule
